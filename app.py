b6990892e6d89cc4f745020585028048836a9259
okakyouhei <kanayaakasa958@gmail.com>
2023-12-18 22:52:40 +0900

    error

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1fafdd059fde54763adb12b312914e1822559ca6de96f7403414ff9cddb55bb7
-size 715
+#-*- coding: utf-8 -*-
+import os
+from flask import (
+    Flask,
+    request,
+    render_template,
+    Response,
+    url_for,
+    redirect
+)
+from model import voiceRemove, mp4ToMp3
+
+app = Flask(__name__)
+
+UPLOAD_VIDEO_FOLDER = './static/video'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        upload_file = request.files['upload_file']
+        video_path = os.path.join(UPLOAD_VIDEO_FOLDER, upload_file.filename)
+        upload_file.save(video_path)
+        voiceRemove(video_path, "input.mp3")
+        return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
